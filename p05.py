fab0709b0582a4e5cef727b3f077d553b941b983
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-05-16 21:03:27 +0200

    Update p05.py

--- p05.py
+++ p05.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
@@ -6,4 +7,21 @@ from bs4 import BeautifulSoup
 if load_dotenv():
     url = os.getenv('URL')
 
-print(url)
+for i in range(50):
+    url = f'https://books.toscrape.com/catalogue/page-{i + 1}.html'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        book_titles = soup.section.find_all('a')
+        for title in book_titles:
+            if title.get('title') is not None:
+                print(title.get('title'))
+                # pohraniti u bazu, datoteku, rjecnik, listu i sl.
+
+        book_prices = soup.find_all(class_='price_color')
+        for price in book_prices:
+            print(price.string.replace('Â', ''))
+            # dodaj uz knjigu u bazu, datoteku, rjecnik, listu i sl.
